f3ccdd04473ebcdf281ff73ddbaf66913842bf9d
LordRaiden <zeyadmo810@gmail.com>
2024-08-07 08:14:00 +0300

    navigation

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <x-layout name="about">
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
     <h1>This is about page</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout name="home">
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
     <h1>This is home page</h1>
 </x-layout>
